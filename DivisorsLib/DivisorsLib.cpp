740c905e913f827fffed900b4ec77de737b57d8c
YoussefNz <youssn6214601_aeh@students.vizja.pl>
2024-10-22 04:32:01 +0100

    FASTER ALGORITHM

--- DivisorsLib/DivisorsLib.cpp
+++ DivisorsLib/DivisorsLib.cpp
@@ -3,11 +3,18 @@
 
 std::vector<unsigned long long> divisors(unsigned long long number)
 {
-	std::vector<unsigned long long> result;
-	for (unsigned long long i = 1; i <= number; ++i)
-	{
-		if (number % i == 0)
-			result.push_back(i);
-	}
-	return result;
+    std::vector<unsigned long long> result;
+    for (unsigned long long i = 1; i * i <= number; ++i)
+    {
+        // Faster algorithm: only iterate up to the square root of number
+        if (number % i == 0)
+        {
+            result.push_back(i); // i is a divisor
+            if (i != number / i) // To avoid adding the same divisor twice
+            {
+                result.push_back(number / i); // number / i is also a divisor
+            }
+        }
+    }
+    return result;
 }
